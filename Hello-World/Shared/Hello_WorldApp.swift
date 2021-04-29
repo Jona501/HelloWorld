c00695073ffc5d7aef798cccef9813461b4fdd50
Jona Ka <jonathan97@cityweb.de>
2021-04-29 22:13:05 +0200

    Init iOS project

---
+++ Hello-World/Shared/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Shared
+//
+//  Created by Jonathan on 29.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
